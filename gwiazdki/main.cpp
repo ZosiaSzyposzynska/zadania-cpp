bb8fa57d66bc23f3bd7875fb0502e05d80fedd2a
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-10 10:55:46 +0200

    Add comment for gwiazdki cpp

--- gwiazdki/main.cpp
+++ gwiazdki/main.cpp
@@ -8,14 +8,14 @@ int main() {
 
     cout << "Podaj liczbe gwiazdek: ";
     cin >> n;
-
+//1 sposob za pomoca petli while
     cout << "Petla while: " << endl;
     int i = 0;
     while (i < n) {
         cout << "*" << endl;
         i++;
     }
-
+//2 sposob za pomoca petli for
     cout << "Petla for: " << endl;
     for (int j = 0; j < n; j++) {
         cout << "*" << endl;
